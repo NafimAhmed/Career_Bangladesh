78b1a47fbc35a3f8d1e598fba7ae3c4f31b349bd
Nafim Ahmed <recentnafimahmed@gmail.com>
2023-01-12 18:19:31 +0600

    job confirmation start

--- app/src/main/java/com/nafim/jobportal/Employer_JobConformation_page.java
+++ app/src/main/java/com/nafim/jobportal/Employer_JobConformation_page.java
@@ -10,5 +10,14 @@ public class Employer_JobConformation_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employer_job_conformation_page);
+
+        Bundle extras = getIntent().getExtras();
+
+        String jobTitle,employer,deadLine,eduQualification;
+        jobTitle= extras.getString("jobType");
+        employer=extras.getString("jobTitle");
+        deadLine=extras.getString("empjobLocation");
+        eduQualification=extras.getString("emp_educationalQualification");
+
     }
 }
--- app/src/main/java/com/nafim/jobportal/Emplyer_job_Add_Page.java
+++ app/src/main/java/com/nafim/jobportal/Emplyer_job_Add_Page.java
@@ -2,16 +2,19 @@ package com.nafim.jobportal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class Emplyer_job_Add_Page extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner jobTypeSpiner;
+    EditText empjobTitle,empjobLocation,emp_educationalQualification,empJobResponsiblity,Salary,deadline;
     Button confirmBtn;
 
     @Override
@@ -24,11 +27,30 @@ public class Emplyer_job_Add_Page extends AppCompatActivity implements AdapterVi
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         jobTypeSpiner.setAdapter(adapter);
         jobTypeSpiner.setOnItemSelectedListener(this);
+        empjobTitle=findViewById(R.id.empjobTitle);
+        empjobLocation=findViewById(R.id.empjobLocation);
+        emp_educationalQualification=findViewById(R.id.emp_educationalQualification);
+        empJobResponsiblity=findViewById(R.id.empJobResponsiblity);
+        Salary=findViewById(R.id.Salary);
+        deadline=findViewById(R.id.deadline);
 
         confirmBtn=findViewById(R.id.confirmPost);
         confirmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+
+                Intent intent = new Intent(getApplicationContext(),Employer_JobConformation_page.class);
+                intent.putExtra("jobType",jobTypeSpiner.getSelectedItem().toString());
+                intent.putExtra("jobTitle",empjobTitle.getText().toString());
+                intent.putExtra("empjobLocation",empjobLocation.getText().toString());
+                intent.putExtra("emp_educationalQualification",emp_educationalQualification.getText().toString());
+                intent.putExtra("empJobResponsiblity",empJobResponsiblity.getText().toString());
+                intent.putExtra("Salary",Salary.getText().toString());
+                intent.putExtra("deadline",deadline.getText().toString());
+                startActivity(intent);
+
 
 
             }
