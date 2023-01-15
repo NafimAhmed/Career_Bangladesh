bd0326a0ee36a088e2b7e74a94874dec5b5d6948
Nafim Ahmed <recentnafimahmed@gmail.com>
2023-01-15 11:35:10 +0600

    employer layout done

--- app/src/main/java/com/nafim/jobportal/Employer_JobConformation_page.java
+++ app/src/main/java/com/nafim/jobportal/Employer_JobConformation_page.java
@@ -2,13 +2,17 @@ package com.nafim.jobportal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Employer_JobConformation_page extends AppCompatActivity {
 
     TextView jobTitleDetail,locationDetail,employerDetail,vacancyDetail,SalaryDetail,deadlineDetail,educationalQualificationDetail;
 
+    Button btnPost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +24,20 @@ public class Employer_JobConformation_page extends AppCompatActivity {
         jobTitleDetail=findViewById(R.id.jobTitleDetail);
         employerDetail=findViewById(R.id.employerDetail);
         locationDetail=findViewById(R.id.locationDetail);
-        vacancyDetail=findViewById(R.id.vacancyDetail);
+        //vacancyDetail=findViewById(R.id.vacancyDetail);
         SalaryDetail=findViewById(R.id.SalaryDetail);
         deadlineDetail=findViewById(R.id.deadlineDetail);
         educationalQualificationDetail=findViewById(R.id.educationalQualificationDetail);
+
+        btnPost=findViewById(R.id.btnpost);
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Done_page.class);
+                startActivity(intent);
+            }
+        });
 
 
 
@@ -33,16 +47,29 @@ public class Employer_JobConformation_page extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
 
-        String jobTitle,empjobvacancy,employer,deadLine,eduQualification,employerName,empJobResponsiblity,Salary,deadline;
-        jobTitle= extras.getString("jobType");
-        employer=extras.getString("jobTitle");
-        deadLine=extras.getString("empjobLocation");
+        String jobTitle,jobType,empjobvacancy,jobLocation,employer,deadLine,eduQualification,employerName,empJobResponsiblity,Salary,deadline;
+        jobTitle= extras.getString("jobTitle");
+        jobType=extras.getString("jobType");
+        employer=extras.getString("employerName");
+
         eduQualification=extras.getString("emp_educationalQualification");
         empJobResponsiblity= extras.getString("empJobResponsiblity");
         empjobvacancy=extras.getString("empjobvacancy");
+        jobLocation=extras.getString("empjobLocation");
         Salary=extras.getString("Salary");
         deadline= extras.getString("deadline");
         employerName=extras.getString("employerName");
 
+
+        jobTitleDetail.setText(jobTitle);
+        employerDetail.setText(employer);
+        deadlineDetail.setText(deadline);
+        locationDetail.setText(jobLocation);
+        SalaryDetail.setText(Salary);
+        vacancyDetail.setText(empjobvacancy);
+        deadlineDetail.setText(deadline);
+        educationalQualificationDetail.setText(eduQualification);
+
     }
+
 }
