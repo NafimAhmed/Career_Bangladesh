9a2b2d34dacef7a0ccfc786bc02db27c85a0c051
recen <recentnafimahmed@gmail.com>
2023-07-12 12:15:58 +0600

    profile repair

--- app/src/main/java/com/careers/app/Education_Profile.java
+++ app/src/main/java/com/careers/app/Education_Profile.java
@@ -2,13 +2,64 @@ package com.careers.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Education_Profile extends AppCompatActivity {
+
+
+    Button addedu;
+
+    EditText edulevelOne,eduResultFour,eduResultTypeSpinnerFour,eduInstituteFour,PassingyearFour,eduMajorTwoSpinnerFour,edulevelTFour,eduResultThree,eduResultTypeSpinnerThree
+            ,PassingYearThree,eduInstituteThree,eduMajorSpinnerThree,edulevelThree,eduResultTwo,
+            eduResultTypeSpinnerTwo,PassingYearTwo,eduInstituteTwo,edulevelTwo,eduResultOne,eduPassingYearOne,
+            eduInstituteOne,eduMajorSpinnerOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_education_profile);
+
+
+
+
+        edulevelOne=findViewById(R.id.edulevelOne);
+        eduMajorSpinnerOne=findViewById(R.id.eduMajorSpinnerOne);
+        eduInstituteOne=findViewById(R.id.eduInstituteOne);
+        eduPassingYearOne=findViewById(R.id.eduPassingYearOne);
+        eduResultOne=findViewById(R.id.eduResultOne);
+        edulevelTwo=findViewById(R.id.edulevelTwo);
+        eduInstituteTwo=findViewById(R.id.eduInstituteTwo);
+        PassingYearTwo=findViewById(R.id.PassingYearTwo);
+        eduResultTypeSpinnerTwo=findViewById(R.id.eduResultTypeSpinnerTwo);
+        eduResultTwo=findViewById(R.id.eduResultTwo);
+        edulevelThree=findViewById(R.id.edulevelThree);
+        eduMajorSpinnerThree=findViewById(R.id.eduMajorSpinnerThree);
+        eduInstituteThree=findViewById(R.id.eduInstituteThree);
+        PassingYearThree=findViewById(R.id.PassingYearThree);
+        eduResultTypeSpinnerThree=findViewById(R.id.eduResultTypeSpinnerThree);
+        eduResultThree=findViewById(R.id.eduResultThree);
+        edulevelTFour=findViewById(R.id.edulevelTFour);
+        eduMajorTwoSpinnerFour=findViewById(R.id.eduMajorTwoSpinnerFour);
+        eduInstituteFour=findViewById(R.id.eduInstituteFour);
+        PassingyearFour=findViewById(R.id.PassingyearFour);
+        eduResultTypeSpinnerFour=findViewById(R.id.eduResultTypeSpinnerFour);
+        eduResultFour=findViewById(R.id.eduResultFour);
+
+
+
+
+        addedu=findViewById(R.id.addedu);
+
+        addedu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Exp_profile.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/careers/app/Exp_profile.java
+++ app/src/main/java/com/careers/app/Exp_profile.java
@@ -2,13 +2,55 @@ package com.careers.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Exp_profile extends AppCompatActivity {
+
+    EditText CompanyOneName,jbLocationTwo,jbExpertiesTwo,jbResponsiblityTwo,resigningDateTwo,JoiningDateTwo,CompanyTwoDepartment,CompanyTwoDesignetion,copanyTwoBusiness,CompanyTwoName,jbLocationOne,jbExpertiesOne,copanyOneBusiness,CompanyOneDesignetion,jbResponsiblityOne,resigningDateOne,CompanyOneDepartment,JoiningDateOne;
+
+    Button addexp;//
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exp_profile);
+
+
+
+        CompanyOneName=findViewById(R.id.CompanyOneName);
+        copanyOneBusiness=findViewById(R.id.copanyOneBusiness);
+        CompanyOneDesignetion=findViewById(R.id.CompanyOneDesignetion);
+        CompanyOneDepartment=findViewById(R.id.CompanyOneDepartment);
+        JoiningDateOne=findViewById(R.id.JoiningDateOne);
+        resigningDateOne=findViewById(R.id.resigningDateOne);
+        jbResponsiblityOne=findViewById(R.id.jbResponsiblityOne);
+        jbExpertiesOne=findViewById(R.id.jbExpertiesOne);
+        jbLocationOne=findViewById(R.id.jbLocationOne);
+        CompanyTwoName=findViewById(R.id.CompanyTwoName);
+        copanyTwoBusiness=findViewById(R.id.copanyTwoBusiness);
+        CompanyTwoDesignetion=findViewById(R.id.CompanyTwoDesignetion);
+        CompanyTwoDepartment=findViewById(R.id.CompanyTwoDepartment);
+        JoiningDateTwo=findViewById(R.id.JoiningDateTwo);
+        resigningDateTwo=findViewById(R.id.resigningDateTwo);
+        jbResponsiblityTwo=findViewById(R.id.jbResponsiblityTwo);
+        jbExpertiesTwo=findViewById(R.id.jbExpertiesTwo);
+        jbLocationTwo=findViewById(R.id.jbLocationTwo);
+
+        addexp=findViewById(R.id.addexp);
+
+
+        addexp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),)
+            }
+        });
+
+
     }
 }
